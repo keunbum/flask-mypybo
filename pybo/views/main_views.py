073a954792455f541a3a02779400411c1bb5f6ae
keunbum <rkeunbum@gmail.com>
2022-10-14 19:52:48 +0900

    update main_views

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 from pybo.models import Question
@@ -8,5 +8,5 @@ bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    3/0
+    current_app.logger.info("INFO 레벨로 출력")
     return redirect(url_for('question._list'))
